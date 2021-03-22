b64371731a1d899a952de014fc3c9b894cfb9d1a
unknown <femiabimbola6@gmail.com>
2021-03-22 16:59:33 +0100

    started main branch

---
+++ src/components/Main/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Divider} from '@material-ui/core'
+
+const Main = () => {
+
+  return (
+    <div>
+      The main component
+    </div>
+  )
+}
+
+export default Main;
